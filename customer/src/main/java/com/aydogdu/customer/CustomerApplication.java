e6ede29c813b09148c0d4ed0edb8b2b32b2d0459
ayseaydogdu <ayseaydogdu122@gmail.com>
2022-08-09 19:55:52 +0300

    customer-service debuged

--- customer/src/main/java/com/aydogdu/customer/CustomerApplication.java
+++ customer/src/main/java/com/aydogdu/customer/CustomerApplication.java
@@ -3,11 +3,13 @@ package com.aydogdu.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class CustomerApplication {
 
     public static void main(String[] args) {
